d396b84447b0f1906e31b3ff589bf5d3833e7d3c
nobodyn0se <48710616+nobodyn0se@users.noreply.github.com>
2019-09-12 23:39:04 +0530

    Update Lyrics App.py
    Added headers to prevent browser-type ambiguity when requesting data, no more HTTP 403 Errors!  Used Request library for creating handles.

--- Lyrics App.py
+++ Lyrics App.py
@@ -1,6 +1,6 @@
 import bs4 as bs
 from googlesearch import search
-import urllib.request
+from urllib.request import Request, urlopen
 
 
 def get_url(query):
@@ -8,10 +8,19 @@ def get_url(query):
         return j
 
 
-def url_parse(url):
-    req = urllib.request.urlopen(url).read()
-    resp = bs.BeautifulSoup(req, features="lxml")
-    for para in resp.find_all('div', attrs={'class': 'holder lyric-box'}):
+def url_parse(url1):
+    hdr = {
+        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+        'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
+        'Accept-Encoding': 'none',
+        'Accept-Language': 'en-US,en;q=0.8',
+        'Connection': 'keep-alive'}
+    
+    req = Request(url=url1, headers=hdr)
+    wb = urlopen(req).read()
+    resp = bs.BeautifulSoup(wb, 'html.parser')
+    for para in resp.find_all('div', attrs={'class': 'lyrics'}):
         print(para.text)
 
 
